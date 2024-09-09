bf7fffc4a095bad7976cb50bda4d59086a8245d7
Vamshi-27 <vamshira18@outlook.com>
2024-09-09 20:37:57 +0530

    sync

---
+++ Sum.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Enter the two numbers:\n";
+    cin>>a>>b;
+    cout<<"The addition of a and b is "<<(a+b)<<endl;
+    return 0;
+}
